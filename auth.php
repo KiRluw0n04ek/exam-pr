126e29f4e7971b0672be428e4cdbb09d80625a9e
Kyryl <125684765+KiRluw0n04ek@users.noreply.github.com>
2023-11-14 10:36:24 +0200

    Delete auth.php

--- auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-  if($_COOKIE['User'] == 'YES')
-    setcookie('User', 'YES', time() - 3600, '/' );
-  else
-    setcookie('User', 'YES', time() + 3600, '/' );
-
-  header('Location: /');
- ?>
